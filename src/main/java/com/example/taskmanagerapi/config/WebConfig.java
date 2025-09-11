2945c63774620cd57138bccaca0dabf26385c3d6
Gloria-Obiorah <obiorahgloria911@gmail.com>
2025-09-11 20:21:44 +0100

    web config

--- src/main/java/com/example/taskmanagerapi/config/WebConfig.java
+++ src/main/java/com/example/taskmanagerapi/config/WebConfig.java
@@ -3,15 +3,14 @@ package com.example.taskmanagerapi.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500","http://localhost:5500","http://127.0.0.1:5500/register.html")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
