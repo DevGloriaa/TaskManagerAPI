bbf487023f05f3eca3b4500ed43db526d9c9e65a
Gloria-Obiorah <obiorahgloria911@gmail.com>
2025-09-15 22:58:15 +0100

    App config

--- src/main/java/com/example/taskmanagerapi/TaskManagerApiApplication.java
+++ src/main/java/com/example/taskmanagerapi/TaskManagerApiApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Value;
 import jakarta.annotation.PostConstruct;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TaskManagerApiApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/taskmanagerapi/serviceImpl/ServerServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.taskmanagerapi.serviceImpl;
+
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ServerServiceImpl {
+    private final String serverUrl = "https://taskmanagerapi-1-142z.onrender.com";
+
+    private final RestTemplate restTemplate;
+
+    public ServerServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Scheduled(fixedRate = 600000)
+    public void serverServiceImpl() {
+        try {
+            restTemplate.getForObject(serverUrl, String.class);
+            System.out.println("Pinged server successfully at " + System.currentTimeMillis());
+        } catch (Exception e) {
+            System.err.println("Failed to ping server: " + e.getMessage());
+        }
+    }
+
+}
