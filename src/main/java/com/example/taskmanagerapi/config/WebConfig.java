f7463c8b6acd554058b72efe5a38521bda8f6ff3
Gloria-Obiorah <obiorahgloria911@gmail.com>
2025-10-07 21:58:15 +0100

    fix: Allow localhost only in dev/local profiles

--- src/main/java/com/example/taskmanagerapi/config/WebConfig.java
+++ src/main/java/com/example/taskmanagerapi/config/WebConfig.java
@@ -2,18 +2,11 @@ package com.example.taskmanagerapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
-    private final Environment env;
-
-    public WebConfig(Environment env) {
-        this.env = env;
-    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -21,23 +14,15 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
-
-                String[] allowedOrigins;
-                if (env.acceptsProfiles("dev", "local")) {
-                    allowedOrigins = new String[]{
-                            "http://localhost:3000",
-                            "https://optimustask.vercel.app"
-                    };
-                } else {
-                    allowedOrigins = new String[]{
-                            "https://optimustask.vercel.app"
-                    };
-                }
+                String[] allowedOrigins = new String[]{
+                        "http://localhost:3000",          
+                        "https://optimustask.vercel.app"
+                };
 
                 registry.addMapping("/**")
                         .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                        .allowedHeaders("Authorization", "Content-Type")
+                        .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
